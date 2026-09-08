e24f72d2052a91a59d073b76dc2bcbdf9a7466e5
RajKashyap-20 <rajkashyap.ki05@gmail.com>
2026-09-08 17:06:33 +0530

    Naam Gyan Project.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? "/NaamKatha/" : "/"
+
+  base: process.env.GITHUB_ACTIONS
+    ? "/naam-gyan/"
+    : "/"
 });
